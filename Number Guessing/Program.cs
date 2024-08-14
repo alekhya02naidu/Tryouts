9275426ee0244f15630fd275f83adf95533d577d
alekhya02naidu <alekhyalsvnaidu@gmail.com>
2024-08-14 18:16:31 +0530

    number gueesing game with loops, conditional statements

---
+++ Number Guessing/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace GuessingNumber;
+
+public static class GuessTheNumber
+{
+    public static void Main(string[] args)
+    {
+        Random random = new Random(); //Random
+        int numberToBeGuessed = random.Next(1, 101); //stores a random number ranges from 1 to 100
+        int userGuess = 0;
+        int attempts = 0;
+
+        Console.WriteLine("Guess the number between 1 to 100");
+        while(numberToBeGuessed != userGuess)
+        {
+            userGuess = Convert.ToInt32(Console.ReadLine());
+            if(userGuess == numberToBeGuessed)
+            {
+                Console.WriteLine("Congratulations! You have guessed correctly in "+ attempts+" attempts");
+            }
+            else if(userGuess > numberToBeGuessed)
+            {
+                Console.WriteLine("Its High! Try again");
+            }
+            else
+            {
+                Console.WriteLine("Its Low! Try again");
+            }
+            attempts +=1; 
+        }
+    }
+}
